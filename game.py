e5542968ca2bf3a1f30a5714da412ee162d60bd5
tonylearn09 <tonyhung09@gmail.com>
2018-04-28 02:20:31 +0800

    fix valid_pos error

--- game.py
+++ game.py
@@ -76,7 +76,7 @@ class Gomoku:
         # update a board given a pos
         # pos is in the form 0~216
         pos_index = self.index_map[pos]
-        if self.gomokuboard[pos_index[0]][pos_index[1]] == 0:
+        if self.gomokuboard[pos_index[0]][pos_index[1]] != 0:
             # No update
             return False
         self.totalSteps[self.nextPlayer - 1] += 1
@@ -138,7 +138,7 @@ class Gomoku:
             j = self.lastMove[1]
             while boardState((i, j)) == lastPlayer:
                 count[d] += 1
-                if i <= 8:
+                if i < 8:
                     # top half
                     i += direction[d][0]
                     j += direction[d][1]
@@ -186,8 +186,13 @@ def updateFeature(gomoku, pos_index):
                        if nextMove_j-delta >= 0]
 
     # valid positions for checking
-    valid_pos = copy.deepcopy(startPos['row'])
+    #valid_pos = copy.deepcopy(startPos['row'])
+    valid_pos = set()
+    valid_pos.update(startPos['row'])
     startPos['row'] = startPos['row'][-1]
+
+    valid_pos.update([(nextMove_i, nextMove_j+delta) for delta in range(0, 5) \
+                      if nextMove_j+delta < len(gomokuboard[nextMove_i])])
 
     if nextMove_i <= 8:
         # top half (head)
@@ -196,8 +201,10 @@ def updateFeature(gomoku, pos_index):
         startPos['rdiag'] = [(nextMove_i-delta, nextMove_j) for delta in range(0, 5) \
                                 if nextMove_i-delta>= 0]
 
-        valid_pos += copy.deepcopy(startPos['diag'])
-        valid_pos += copy.deepcopy(startPos['rdiag'])
+        #valid_pos += copy.deepcopy(startPos['diag'])
+        valid_pos.update(startPos['diag'])
+        #valid_pos += copy.deepcopy(startPos['rdiag'])
+        valid_pos.update(startPos['rdiag'])
         startPos['diag'] = startPos['diag'][-1]
         startPos['rdiag'] = startPos['rdiag'][-1]
 
@@ -207,57 +214,71 @@ def updateFeature(gomoku, pos_index):
             if nextMove_i + delta > 8:
                 y_index = nextMove_j + (8 - nextMove_i)
                 if y_index < len(gomoku.gomokuboard[nextMove_i+delta]):
-                    valid_pos.append((nextMove_i+delta, y_index))
+                    #valid_pos.append((nextMove_i+delta, y_index))
+                    valid_pos.add((nextMove_i+delta, y_index))
             else:
                 if nextMove_j+delta < len(gomoku.gomokuboard[nextMove_i+delta]):
-                    valid_pos.append(nextMove_i+delta, nextMove_j+delta)
+                    #valid_pos.append(nextMove_i+delta, nextMove_j+delta)
+                    valid_pos.add((nextMove_i+delta, nextMove_j+delta))
         # rdiag
         for delta in range(0, 5):
             if nextMove_i + delta > 8:
                 y_index = nextMove_j - (delta - (8 - nextMove_i))
                 if y_index >= 0:
-                    valid_pos.append((nextMove_i+delta, y_index))
+                    #valid_pos.append((nextMove_i+delta, y_index))
+                    valid_pos.add((nextMove_i+delta, y_index))
             else:
                 if nextMove_j-delta >= 0:
-                    valid_pos.append(nextMove_i+delta, nextMove_j-delta)
+                    #valid_pos.append(nextMove_i+delta, nextMove_j-delta)
+                    valid_pos.add((nextMove_i+delta, nextMove_j-delta))
 
 
     elif nextMove_i > 8:
         # bottom half (head)
         start_diag_tuple = pos_index
-        valid_pos.append(start_diag_tuple)
+        #valid_pos.append(start_diag_tuple)
+        valid_pos.add(start_diag_tuple)
         for delta in range(0, 5):
             if nextMove_i - delta > 8:
                 start_diag_tuple = (nextMove_i-delta, nextMove_j)
-                valid_pos.append(start_diag_tuple)
+                #valid_pos.append(start_diag_tuple)
+                valid_pos.add(start_diag_tuple)
             else:
                 col_index = nextMove_j - (delta - (nextMove_i - 8))
                 if col_index >= 0:
                     start_diag_tuple = (nextMove_i-delta, col_index)
-                    valid_pos.append(start_diag_tuple)
+                    #valid_pos.append(start_diag_tuple)
+                    valid_pos.add(start_diag_tuple)
 
         startPos['diag'] = start_diag_tuple
 
         start_rdiag_tuple = pos_index
-        valid_pos.append(start_rdiag_tuple)
+        #valid_pos.append(start_rdiag_tuple)
+        valid_pos.add(start_rdiag_tuple)
         for delta in range(0, 5):
             if nextMove_i - delta >= 8:
                 if nextMove_j + delta < len(gomoku.gomokuboard[nextMove_i-delta]):
                     start_rdiag_tuple = (nextMove_i-delta, nextMove_j+delta)
-                    valid_pos.append(start_rdiag_tuple)
+                    #valid_pos.append(start_rdiag_tuple)
+                    valid_pos.add(start_rdiag_tuple)
             else:
                 col_index = nextMove_j + (nextMove_i - 8)
                 if col_index < len(gomoku.gomokuboard[nextMove_i-delta]):
                     start_rdiag_tuple = (nextMove_i-delta, col_index)
-                    valid_pos.append(start_rdiag_tuple)
+                    #valid_pos.append(start_rdiag_tuple)
+                    valid_pos.add(start_rdiag_tuple)
 
         startPos['rdiag'] = start_rdiag_tuple
 
         # bottome half (tail)
-        valid_pos += [(nextMove_i+delta, nextMove_j) for delta in range(0, 5) \
-                          if nextMove_i+delta < 18]
-        valid_pos += [(nextMove_i+delta, nextMove_j-delta) for delta in range(0, 5) \
-                          if (nextMove_i+delta < 18 and nextMove_j-delta >= 0)]
+        #valid_pos += [(nextMove_i+delta, nextMove_j) for delta in range(0, 5) \
+                          #if nextMove_i+delta < 18]
+        valid_pos.update([(nextMove_i+delta, nextMove_j) for delta in range(0, 5) \
+                          if nextMove_i+delta < 18])
+        #valid_pos += [(nextMove_i+delta, nextMove_j-delta) for delta in range(0, 5) \
+                          #if (nextMove_i+delta < 18 and nextMove_j-delta >= 0)]
+        valid_pos.update([(nextMove_i+delta, nextMove_j-delta) for delta in range(0, 5) \
+                          if (nextMove_i+delta < 18 and nextMove_j-delta >= 0)])
     
     direction = {'row': (0, 1), 'diag': (1, 1), 'rdiag': (1, 0),
                  'row_b': (0, 1), 'diag_b': (1, 0), 'rdiag_b': (1, -1)}
@@ -269,14 +290,18 @@ def updateFeature(gomoku, pos_index):
         prev_history = deque()
         prev_history.append((-1, -1)) #dummy
         prev_history.append((i, j))
+        #print(d)
+        #print(valid_pos)
         while (i, j) in valid_pos:
+            if d == 'diag':
+                print(windowCount)
             if initial < windowSize - 1:
                 if gomoku.gomokuboard[i][j] == agent:
                     windowCount['agent'] += 1
                 elif gomoku.gomokuboard[i][j] == opponent:
                     windowCount['opponent'] += 1
                 initial += 1
-                if i <= 8:
+                if i < 8:
                     # top half
                     i += direction[d][0]
                     j += direction[d][1]
@@ -285,8 +310,11 @@ def updateFeature(gomoku, pos_index):
                     i += direction[d+'_b'][0]
                     j += direction[d+'_b'][1]
 
+
                 prev_history.append((i, j))
                 continue
+
+
             if (gomoku.gomokuboard[i][j] == agent):
                 windowCount['agent'] += 1
             elif (gomoku.gomokuboard[i][j] == opponent):
@@ -307,7 +335,7 @@ def updateFeature(gomoku, pos_index):
             if (windowCount['agent'] <= 0):
                 opponentCount[windowCount['opponent']] -= 1
 
-            if i <= 8:
+            if i < 8:
                 # top half
                 i += direction[d][0]
                 j += direction[d][1]
@@ -324,4 +352,6 @@ def updateFeature(gomoku, pos_index):
 
 if __name__ == "__main__":
     g = Gomoku(1)
-    g.print_board()
+    #g.print_board()
+    g.updateBoard(100)
+    print(g.winningCount)
