7ab9075ebee47f6cca77874e2412fd3b59c72733
tonylearn09 <tonyhung09@gmail.com>
2018-04-29 03:25:36 +0800

    Add TD learning

--- cpp/TdLearner.h
+++ cpp/TdLearner.h
@@ -8,14 +8,13 @@ using namespace std;
 
 class TdLearner {
     public:
-        TdLearner(Gomoku game, float eta = 0.01, float gamma = 0.9);
+        TdLearner(float eta = 0.01, float gamma = 0.9);
         void learning(int numTrails = 50, 
                 map<int, float> weights = {{1,1.0}, {2,2.0}, {3,3.0}, {4,4.0}, {5,5.0}});
 
     private:
         float eta_;
         float gamma_;
-        Gomoku game_;
 };
 
 #endif
