3bb2b37d7df450189206edc359d916d54695fa4f
tonylearn09 <tonyhung09@gmail.com>
2018-04-28 16:33:28 +0800

    Add RandomPolicy

---
+++ cpp/RandomPolicy.cpp
@@ -0,0 +1,17 @@
+#include "RandomPolicy.h"
+
+using namespace std;
+
+int RandomPolicy::getNextAction(Gomoku game) {
+    int agent = game.nextPlayer;
+    int op = 3 - agent;
+    while (1) { 
+        pos = rand() % static_cast<int>(217);
+        tuple<int, int> pair = game.index_map[pos];
+        int x = get<0>(pair);
+        int y = get<1>(pair);
+
+        if (game.gomokuboard[x][y] == 0)
+            return pos;
+    }
+
---
+++ cpp/RandomPolicy.h
@@ -0,0 +1,13 @@
+#ifndef RANDOM_POLICY_H
+#define RANDOM_POLICY_H
+
+#include "Gomoku.h"
+
+using namespace std;
+
+class RandomPolicy {
+    public:
+        int getNextAction(Gomoku game);
+};
+
+#endif
